01a41e5fea152bc7793c534b766a75c250358ffb
MonashJeremia <162404299+MonashJeremia@users.noreply.github.com>
2024-04-03 15:22:07 +1100

    Added regen and get_item
    Tried regen, v confused, gave up
    Implemented get_item

--- poke_team.py
+++ poke_team.py
@@ -15,29 +15,23 @@ class PokeTeam:
 
     """
     Best case for choose_manually is is O(1), if the user is 'done' and stores no pokemon
-    Worst case for choose_manually is O(n) as it iterates over user pokemon choices, appending
-    valid options to self.team until the team limit has been reached the user is done
+    Worst case for choose_manually is O(n), where n the number of user inputs. As it iterates 
+    over user pokemon choices, appending valid options to self.team until the team limit has 
+    been reached the user is done
     """
     def choose_manually(self):
-        # Initialise array size
-        self.team = ArrayR(1)
+        self.team = ArrayR(1) # Initialise array size
 
-        # While team Array is less TEAM_LIMIT (6)
-        while len(self.team) < PokeTeam.TEAM_LIMIT:
-            # Requests user pokemon choices
-            pokemon_choice = input("Enter a Pokemon's name or 'done': ")
-            # If user requests done, break
-            if pokemon_choice.lower() == 'done':
+        while len(self.team) < PokeTeam.TEAM_LIMIT: # While team Array is less TEAM_LIMIT (6)
+            pokemon_choice = input("Enter a Pokemon's name or 'done': ") # Requests user pokemon choices
+            if pokemon_choice.lower() == 'done': # If user requests done, break
                 break
-            # If user chose a pokemon in the pokemon list
-            if pokemon_choice in PokeTeam.POKE_LIST:
-                # Append pokemon to the array and increase team size by 1
-                for i in range(len(self.team)):
+            if pokemon_choice in PokeTeam.POKE_LIST: # If user chose a pokemon in the pokemon list
+                for i in range(len(self.team)): # Append pokemon to the array and increase team size by 1
                     self.team[i] = pokemon_choice
                     self.team += 1
             else:
-                # If user inputs invalid pokemon, try again until done or reached team limit
-                print("Inavlid Pokemon name, try again")
+                print("Inavlid Pokemon name, try again") # If user inputs invalid pokemon, try again until done or reached team limit
 
 
     def choose_randomly(self) -> None:
@@ -48,26 +42,18 @@ class PokeTeam:
             self.team[i] = all_pokemon[rand_int]()
             self.team_count += 1
 
-    
+
     def regenerate_team(self, battle_mode: BattleMode, criterion: str = None) -> None:
         """
-           '''
-        O(n), n = team size
-        '''
-           ##redo
-        self.team.reset()
-        self.sort_descending = True
-        monster = self.original_team.get_head()
-        while monster is not None:
-            self.add_to_team((monster.get())())
-            monster = monster.next()
+        Best and Worse Case is O(n), where n is the length of team
         """
-        raise NotImplementedError
-
-
+        # Heals all Pokémon to their original HP
+        for pokemon in self.team:
+            # pokemon.health = Pokemon.health
+            pokemon.health = Pokemon.health # This does not work
 
     def assign_team(self, criterion: str = None) -> None:
-        
+        #Next Task
         raise NotImplementedError
 
     def assemble_team(self, battle_mode: BattleMode) -> None:
@@ -79,17 +65,23 @@ class PokeTeam:
         raise NotImplementedError
 
     def __getitem__(self, index: int):
-        raise NotImplementedError
+        poke_team = self.team # Makes poke_team the array
+        index = int(input("Enter an index to retrieve a Pokemon: ")) # Ask user input what index
+        try:
+            pokemon = poke_team[index] # Tries to see if user input index is within list
+            print(f"The Pokemon at index {index} is: {pokemon}") #print
+        except IndexError: # If not in list, raise error
+            print("Invalid index. Please enter an index within the range of the team.")
+        
 
     def __len__(self):
-        return len(self.team)
-
+        return len(self.team) #returns length of team
 
     def __str__(self):
         team_str = "Pokemon Team:\n"
+        # Displays all pokemon in the current team
         for pokemon in self.team:
             team_str += f"{pokemon.name}\n"
-            
         return team_str
         
 
