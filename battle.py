f3650b081252aec6c283361b2ef279e928bbdbee
MonashJeremia <162404299+MonashJeremia@users.noreply.github.com>
2024-04-08 13:43:10 +1000

    LOTS OF BATTLE()
    made a few functions for battle logic
    implemented set_battle
    need to implement rotate and optimise battle

--- battle.py
+++ battle.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from poke_team import Trainer, PokeTeam
 from typing import Tuple
 from battle_mode import BattleMode
+from math import *
 
 class Battle:
 
@@ -12,7 +13,7 @@ class Battle:
         self.criterion = criterion
 
     def commence_battle(self) -> Trainer | None:
-        if self.battle_mode == BattleMode.SET:
+        if self.battle_mode == BattleMode.SET: # Select BattleMode via enum
             return self.set_battle()
         elif self.battle_mode == BattleMode.ROTATE:
             return self.rotate_battle()
@@ -20,20 +21,132 @@ class Battle:
             return self.optimise_battle()
         else:
             raise ValueError("Please select valid BattleMode")
-    
+        
     def _create_teams(self) -> None:
-        self.trainer_1 = PokeTeam.assemble_team
-        self.trainer_2 = PokeTeam.assemble_team
+        self.trainer_1.poke_team.assemble_team(self.battle_mode) #assemble teams to t1 and t2
+        self.trainer_2.poke_team.assemble_team(self.battle_mode)
+
+    def set_round_start(self):
+        t1_pokemon = self.trainer_1.poke_team.team[-1] # Intialises last index in team to be current pokemon for t1
+        t2_pokemon = self.trainer_2.poke_team.team[-1] # Intialises last index in team to be current pokemon for t2
+
+        if t1_pokemon.speed > t2_pokemon.speed: # If t1 speed is faster than t2 speed
+            self.battle_round(t1_pokemon, t2_pokemon) # t1 is attacker and t2 is defender
+        elif t2_pokemon.speed > t1_pokemon.speed: # Vice versa
+            self.battle_round(t2_pokemon, t1_pokemon)
+        else:
+            self.simultaneous_attack(t1_pokemon, t2_pokemon) # If both same speed, then call simultaneous_attack()
+
+        self.set_round_end(t1_pokemon, t2_pokemon)
+
+    def set_round_end(self, t1_pokemon, t2_pokemon):
+        if t1_pokemon.is_alive() and t2_pokemon.is_alive(): # If both Pokemon alive, -1 hp from current hp
+            t1_pokemon.get_health = t1_pokemon.get_health() - 1 # Gets current hp of pokemon and decrease by -1hp
+            t2_pokemon.get_health = t2_pokemon.get_health() - 1
+
+        if not t1_pokemon.is_alive() and t2_pokemon.is_alive(): # If t2_pokemon is alive and other is not 
+            t2_pokemon.level_up() # Level up t2_pokemon
+
+        elif t1_pokemon.is_alive() and not t2_pokemon.is_alive(): # If t1_pokemon is alive and other is not 
+            t1_pokemon.level_up() # Level up t1_pokemon
+        elif not t1_pokemon.is_alive() and not t2_pokemon.is_alive(): # If both pokemon die
+            pass # Nothing happens
+
+    def check_usable_team(self) -> bool:
+        usable_team = any(pokemon.is_alive() for pokemon in self) # If atleast one pokemon is alive in the team 
+        return usable_team # Return boolean
+    
+    def battle_round(self, attacker, defender) -> None: # Attacker intial turn
+        attack_damage = ceil(attacker.attack(defender) * (self.trainer_1.get_pokedex_completion() / self.trainer_2.get_pokedex_completion())) # Calculates new attack damage from the pokemon's attack multiplied by the ratio of the attackers and defenders pokedex completion
+        defender.defend(attack_damage) # Decrease the defender (pokemon) hp by the newly calculated attack
+
+        if defender.is_alive(): # Defender Turn if pokemon still alive
+            counter_attack_damage = ceil(defender.attack(attacker) * (self.trainer_2.get_pokedex_completion() / self.trainer_1.get_pokedex_completion()))
+            attacker.defend(counter_attack_damage)
+
+    def simultaneous_attack(self, pokemon_1, pokemon_2) -> None: # Both pokemon attack each other
+        attack_damage_1 = ceil(pokemon_1.battle_power(pokemon_2) * (self.trainer_1.get_pokedex_completion() / self.trainer_2.get_pokedex_completion()))
+        attack_damage_2 = ceil(pokemon_2.battle_power(pokemon_1) * (self.trainer_2.get_pokedex_completion() / self.trainer_1.get_pokedex_completion()))
+
+        pokemon_1.defence(attack_damage_2) # Decrease both hp
+        pokemon_2.defence(attack_damage_1)
+
+    def end_battle(self) -> Trainer:
+        if not self.check_usable_team(self.trainer_1.poke_team.team): # If trainer1 has no more pokemon with hp
+            return self.trainer_2  # trainer2 wins
+        elif self.check_usable_team(self.trainer_2.poke_team.team): # Vice versa
+            return self.trainer_1
 
     # Note: These are here for your convenience
     # If you prefer you can ignore them
     def set_battle(self) -> PokeTeam | None:
-        raise NotImplementedError
+        self._create_teams()
+        while self.check_usable_team(self.trainer_1.poke_team.team) and self.check_usable_team(self.trainer_1.poke_team.team): # While there is atleast one alive pokemon in each team
+            t1_pokemon = self.trainer_1.poke_team.team[len(self.trainer_1.poke_team.team) - 1] # Intialises last index in team to be current pokemon for t1
+            t2_pokemon = self.trainer_2.poke_team.team[len(self.trainer_2.poke_team.team) - 1] # Intialises last index in team to be current pokemon for t2
+
+            # Calculate Speeds
+            if t1_pokemon.speed > t2_pokemon.speed: # If t1 speed is faster than t2 speed
+                self.battle_round(t1_pokemon, t2_pokemon) # t1 is attacker and t2 is defender
+            elif t2_pokemon.speed > t1_pokemon.speed: # Vice versa
+                self.battle_round(t2_pokemon, t1_pokemon)
+            else:
+                self.simultaneous_attack(t1_pokemon, t2_pokemon) # If both same speed, then call simultaneous_attack()
+
+            # Checks if pokemons is alive
+            if t1_pokemon.is_alive() and t2_pokemon.is_alive(): # If both Pokemon alive, -1 hp from current hp
+                t1_pokemon.get_health = t1_pokemon.get_health() - 1 # Gets current hp of pokemon and decrease by -1hp
+                t2_pokemon.get_health = t2_pokemon.get_health() - 1
+            
+            if not t1_pokemon.is_alive() and t2_pokemon.is_alive(): # If t2_pokemon is alive and other is not
+                t2_pokemon.level_up() # Level up t2_pokemon
+                self.trainer_1.poke_team.team.pop() # Pops t1_pokemon
+            elif t1_pokemon.is_alive() and not t2_pokemon.is_alive(): # If t1_pokemon is alive and other is not 
+                t1_pokemon.level_up() # Level up t1_pokemon
+                self.trainer_1.poke_team.team.pop() # Pops t2_pokemon
+            elif not t1_pokemon.is_alive() and not t2_pokemon.is_alive(): # If both pokemon die
+                pass # Nothing happens
+        
+        # Checks winner
+        self.end_battle()
+        
 
     def rotate_battle(self) -> PokeTeam | None:
-        raise NotImplementedError
+        self._create_teams()
+        while self.check_usable_team(self.trainer_1.poke_team.team) and self.check_usable_team(self.trainer_1.poke_team.team): # While there is atleast one alive pokemon in each team
+            t1_pokemon = self.trainer_1.poke_team.team[len(self.trainer_1.poke_team.team) - 1] # Intialises last index in team to be current pokemon for t1
+            t2_pokemon = self.trainer_2.poke_team.team[len(self.trainer_2.poke_team.team) - 1] # Intialises last index in team to be current pokemon for t2
+
+            # Calculate Speeds
+            if t1_pokemon.speed > t2_pokemon.speed: # If t1 speed is faster than t2 speed
+                self.battle_round(t1_pokemon, t2_pokemon) # t1 is attacker and t2 is defender
+            elif t2_pokemon.speed > t1_pokemon.speed: # Vice versa
+                self.battle_round(t2_pokemon, t1_pokemon)
+            else:
+                self.simultaneous_attack(t1_pokemon, t2_pokemon) # If both same speed, then call simultaneous_attack()
+
+            # Checks if pokemons is alive
+            if t1_pokemon.is_alive() and t2_pokemon.is_alive(): # If both Pokemon alive, -1 hp from current hp
+                t1_pokemon.get_health = t1_pokemon.get_health() - 1 # Gets current hp of pokemon and decrease by -1hp
+                t2_pokemon.get_health = t2_pokemon.get_health() - 1
+            
+            if not t1_pokemon.is_alive() and t2_pokemon.is_alive(): # If t2_pokemon is alive and other is not
+                t2_pokemon.level_up() # Level up t2_pokemon
+                self.trainer_1.poke_team.team.pop() # Pops t1_pokemon
+            elif t1_pokemon.is_alive() and not t2_pokemon.is_alive(): # If t1_pokemon is alive and other is not 
+                t1_pokemon.level_up() # Level up t1_pokemon
+                self.trainer_1.poke_team.team.pop() # Pops t2_pokemon
+            elif not t1_pokemon.is_alive() and not t2_pokemon.is_alive(): # If both pokemon die
+                pass # Nothing happens
+        # Checks winner
+        self.end_battle()
+
 
     def optimise_battle(self) -> PokeTeam | None:
+        while self.check_usable_team(self.trainer_1.poke_team.team) and self.check_usable_team(self.trainer_1.poke_team.team): # While there is atleast one alive pokemon in each team
+        #write stuff
+        
+        self.end_battle()
         raise NotImplementedError
 
 
